fc4813bac4701d2ff7f2162bc84ac22222afd1ef
calebdeji <33372799+calebdeji@users.noreply.github.com>
2019-11-09 18:53:32 +0100

    I couldn't fix broken git, had to directly drag and upload

---
+++ mechanics/js/app.js
@@ -0,0 +1,216 @@
+let navBarElement = document.querySelector(".navbar");
+let navBarSpan = document.querySelectorAll(".nav-link-span");
+
+/**
+ * To animate the div section on appearing on the vieport
+ */
+// eslint-disable-next-line no-unused-vars
+const animateOnViewPort = () => {
+    let mainSection = document.querySelectorAll(".main__section");
+    mainSection.forEach((item) => {
+        /**
+         * scroll reveal function imported from the script requested in the head tag
+         */
+        // eslint-disable-next-line no-undef
+        ScrollReveal({
+            duration: 1000
+        }).reveal(item, {
+            delay: 300
+        });
+    });
+}
+
+
+/**
+ * Slider effect for the testimonial
+ */
+
+
+let testimonialId;
+let triggerSliderButton = document.querySelectorAll(".slider-trigger-button");
+const automaticSLider = (counter) => {
+    // console.log(counter)
+    let testimonialElements = document.querySelectorAll(".slider__testimonial");
+
+    testimonialElements.forEach(item => {
+        item.style.display = "none";
+    });
+    triggerSliderButton.forEach(item => {
+        item.style.backgroundColor = "rgb(42, 14, 2)";
+    })
+    testimonialElements[counter].style.display = "flex";
+    triggerSliderButton[counter].style.backgroundColor = "#ffffff";
+    counter = (counter <= 1) ? counter + 1 : 0;
+    testimonialId = setTimeout(() => {
+        automaticSLider(counter);
+    }, 3500);
+
+}
+
+/**
+ * screen button clicked effect
+ */
+const screenButtonEffects = () => {
+    let screensButton = document.querySelectorAll(".rounded__anchor-screens");
+    let sliderScreenImageCollection = document.querySelectorAll(".slider__screen-image");
+    for (let screenButtonItem = 0; screenButtonItem < screensButton.length; screenButtonItem++) {
+        for (let sliderScreenImageElement = 0; sliderScreenImageElement < sliderScreenImageCollection.length; sliderScreenImageElement++) {
+            if (screenButtonItem == sliderScreenImageElement) {
+                screensButton[screenButtonItem].addEventListener("click", () => {
+                    screensButton.forEach(item => {
+                        item.classList.remove("active-anchor");
+                    })
+                    sliderScreenImageCollection.forEach((item) => {
+                        item.style.display = "none";
+                    })
+                    screensButton[screenButtonItem].classList.add("active-anchor");
+                    sliderScreenImageCollection[sliderScreenImageElement].style.display = "flex";
+                });
+            }
+        }
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+function slideFirst() {
+    console.log("testimonial id : ", testimonialId);
+    clearInterval(testimonialId);
+
+    // console.log("slider first seen here");
+    // console.log("testimonial id : ", testimonialId);
+    automaticSLider(0);
+}
+
+// eslint-disable-next-line no-unused-vars
+function slideSecond() {
+    clearInterval(testimonialId);
+    automaticSLider(1);
+    // console.log("Slider second seen here");
+}
+
+// eslint-disable-next-line no-unused-vars
+function slideThird() {
+    clearInterval(testimonialId);
+    automaticSLider(2);
+    // console.log("Slider second third here");
+}
+/**
+ * Screen slider
+ */
+let firstSlideScreenButtonLeft = document.querySelector("#screenButtonLeft");
+let firstSlideScreenButtonRight = document.querySelector("#screenButtonRight");
+let secondSlideScreenButtonLeft = document.querySelector("#secondScreenButtonLeft");
+let secondSlideScreenButtonRight = document.querySelector("#secondScreenButtonRight");
+let counter = 0;
+let secondCounter = 0;
+const screenSlider = (determineCounter, index, elementCollection) => {
+    for (let elementCounter = 0; elementCounter < elementCollection.length; elementCounter++) {
+        elementCollection[elementCounter].style.display = "none";
+    }
+    console.log(elementCollection[index]);
+    if (index >= 0 && index < 2) {
+        elementCollection[index].style.display = "flex";
+        if (determineCounter == "counter") {
+            counter++;
+        } else {
+            secondCounter++
+        }
+    } else {
+        index = (index % 2 == 0) ? 0 : 1;
+        elementCollection[index].style.display = "flex";
+        if (determineCounter == "counter") {
+            counter = (counter % 2 == 0) ? 0 : 1;
+            counter++;
+        } else {
+            secondCounter = (secondCounter % 2 == 0) ? 0 : 1;
+            secondCounter++
+        }
+        counter++;
+    }
+
+
+}
+
+
+
+
+
+firstSlideScreenButtonLeft.addEventListener("click", () => {
+    let elementCollection = document.querySelectorAll(".slider__slider-item--screens--first-section");
+    screenSlider("counter", counter - 1, elementCollection);
+});
+
+firstSlideScreenButtonRight.addEventListener("click", () => {
+    let elementCollection = document.querySelectorAll(".slider__slider-item--screens--first-section");
+    screenSlider("counter", counter + 1, elementCollection);
+});
+secondSlideScreenButtonLeft.addEventListener("click", () => {
+    let elementCollection = document.querySelectorAll(".slider__slider-item--screens--second-section");
+    screenSlider("secondCounter", secondCounter - 1, elementCollection);
+});
+secondSlideScreenButtonRight.addEventListener("click", () => {
+    let elementCollection = document.querySelectorAll(".slider__slider-item--screens--second-section");
+    screenSlider("secondCounter", secondCounter + 1, elementCollection);
+})
+
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > navBarElement.offsetTop) {
+        navBarElement.classList.add("navbar--sticky");
+        navBarSpan.forEach((item) => {
+            item.classList.add("nav-link-span--backgroundColor");
+        });
+        document.querySelector(".header").style.paddingTop = "0px";
+    } else {
+        navBarElement.classList.remove("navbar--sticky");
+        navBarSpan.forEach((item) => {
+            item.classList.remove("nav-link-span--backgroundColor");
+        });
+        document.querySelector(".header").style.paddingTop = "40px";
+        // console.log("no hello")
+    }
+});
+window.addEventListener("load", () => {
+    // animateOnViewPort();
+    automaticSLider(0);
+    screenButtonEffects();
+    let navLinkDropDownTrigger = document.getElementById("nav_link_trigger");
+    let dropDownStatus = false;
+    navLinkDropDownTrigger.addEventListener("click", () => {
+        if (dropDownStatus == false) {
+            document.querySelector(".nav-links").classList.add("link-animate");
+            document.querySelector(".nav-links").style.display = "flex";
+            console.log("should effect");
+        } else {
+            document.querySelector(".nav-links").classList.remove("link-animate");
+            document.querySelector(".nav-links").classList.add("link-animate-close");
+
+            setTimeout(() => {
+                document.querySelector(".nav-links").classList.remove("link-animate-close");
+                document.querySelector(".nav-links").style.display = "none";
+
+            }, 900);
+
+        }
+        dropDownStatus = !dropDownStatus;
+    });
+    /**
+     * This is to concatenate string with the 'slide' word in order to reference the funtions with the string name
+     */
+    let wordAssignValue = {
+        0: "First",
+        1: "Second",
+        2: "Third"
+    }
+    let testimonialTriggers = document.querySelectorAll(".slider-trigger-button");
+    for (let triggerCounter = 0; triggerCounter < testimonialTriggers.length; triggerCounter++) {
+
+        testimonialTriggers[triggerCounter].addEventListener("click", () => {
+            let callFunc = `slide` + wordAssignValue[triggerCounter];
+            // `${callFunc= `slide`+wordAssignValue[triggerCounter]}`;
+            let finalCalling = window[callFunc];
+            finalCalling();
+
+        })
+    }
+});
---
+++ mechanics/js/shop.js
@@ -0,0 +1,75 @@
+// eslint-disable-next-line no-undef
+// require('dotenv');
+const itemContainer = document.querySelector(".category-all__item-collection");
+const appendElement = (item) => {
+    const eachItem =
+        `<div class="category-all__each-item category-all__each-item--hover">
+            <div class="category-all__each-item-image-div">
+                <img src = "${item.shop_item_image}" class ="category-all__each-item-image" />
+            </div>
+            <div class="category-all__each-item-text-container">
+                <h3 class="category-all__each-item-text">${item.shop_item_name}</h3>
+                <h5 class="category-all__each-item-text">${item.shop_item_descrpt}</h5>
+                <label for="" class="category-all__each-label">${item.shop_item_price}</label>
+                <p class = "category-all__each-item-text"> Sold By : ${item.shop_item_seller} </p>
+            </div>
+            <div class ="send-message">
+                <button class = "message-button"> Order <i class="fa fa-cart-arrow-down"></i> </button>
+            </div>
+        </div>`;
+    itemContainer.innerHTML += eachItem;
+}
+
+
+const navToogle = (status) => {
+    let linkNavSection = document.querySelector('.main__category');
+    if (status) {
+        linkNavSection.style.display = "flex"
+    } else {
+        linkNavSection.classList.add("slideback");
+        console.log(linkNavSection.className);
+        setTimeout(() => {
+            linkNavSection.style.display = "none";
+            linkNavSection.classList.remove("slideback");
+        }, 200);
+    }
+}
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
+    var firebaseConfig = {
+        apiKey: "AIzaSyBwriMZap33ujMIzx7Ck4EPi5sOrzbGyb0",
+        authDomain: "mechanics-b3612.firebaseapp.com",
+        databaseURL: "https://mechanics-b3612.firebaseio.com",
+        projectId: "mechanics-b3612",
+        storageBucket: "mechanics-b3612.appspot.com",
+        messagingSenderId: "98302440694",
+        appId: "1:98302440694:web:83d23a56461bc6593568d7"
+    };
+    // Initialize Firebase
+
+    // eslint-disable-next-line no-undef
+    firebase.initializeApp(firebaseConfig);
+
+    /**
+     * referencing the database     
+     */
+
+    // eslint-disable-next-line no-undef
+    firebase.database().ref("Shop Collection").on('value', (snapshot) => {
+        snapshot.forEach((item) => {
+            //this calls the function to populate the webpage with the values of the item
+            appendElement(item.val());
+        });
+    });
+
+    let linkNavOpeningTrigger = document.querySelector('.main__link-navigator-trigger');
+    linkNavOpeningTrigger.addEventListener("click", () => {
+        navToogle(true);
+    })
+    let linkNavClosingTrigger = document.querySelector('.main__link-navigator-trigger-close');
+    linkNavClosingTrigger.addEventListener("click", () => {
+        navToogle(false);
+    });
+});
