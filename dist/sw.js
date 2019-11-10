c2d4b6e87a47840d61df758049fd2d7e1b62a48d
calebdeji <calebdeji06@gmail.com>
2019-11-10 17:17:20 +0100

    tired asf

--- dist/sw.js
+++ dist/sw.js
@@ -171,7 +171,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33419" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39273" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
