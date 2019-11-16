1538cc3e897c2448efd6f9ff578b9065aaa2d2fb
calebdeji <calebdeji06@gmail.com>
2019-11-16 19:05:46 +0100

    testing if it will work

--- dist/js/shop.js
+++ dist/js/shop.js
@@ -868,7 +868,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35471" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
