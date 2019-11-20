fc9f9b781476ea9f76a6cd28950ad14f7e6c328e
calebdeji <calebdeji06@gmail.com>
2019-11-20 01:55:20 +0100

    populated successfully

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -363,7 +363,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43975" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45715" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
