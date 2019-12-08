9de075c44c377495dd4cb129fe661656d0d1e48c
calebdeji <calebdeji06@gmail.com>
2019-12-08 18:09:43 +0100

    did some ui changes to the shop page

--- dist/require.49676dcb.js
+++ dist/require.49676dcb.js
@@ -836,7 +836,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41721" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38399" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
